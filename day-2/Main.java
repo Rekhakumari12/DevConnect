2796f676306acdf9ff2c9fa814ed6ce6dbc149a0
rekhakumari-economist <rekhakumari@partner.economist.com>
2025-11-12 10:18:36 +0530

    Java Collections & Java 8 Features

--- day-2/Main.java
+++ day-2/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
   // static - can be access without creating a instance.
@@ -10,8 +12,25 @@ public class Main {
     books.add("Clean Code");
     books.add("Atomic habbits"); // Duplicate allowed
 
-    System.out.println(books);
-    System.out.println(books.get(0));
+    // System.out.println("Filtered books - "+books);
+    // System.out.println(books.get(0));
+    // books.forEach((book)->System.out.println(book));
+    // List<String> squared = books.stream()
+    // .filter(book-> book.startsWith("A"))
+    // .collect(Collectors.toList());
+
+    // System.out.println("Filtered books - "+squared);
+    // List<Integer> nums = Arrays.asList(1,2,3,4);
+    // int sum = nums.stream().reduce(0, (acc,curr) -> acc+curr);
+    // System.out.println("Sum of number - "+sum);
+
+    // Methode reference
+
+    List<String> words = Arrays.asList("Apple", "Banana", "Cherry");
+    words.forEach(System.out::println);
+
+    List<String> upper = words.stream().map(String::toUpperCase).collect(Collectors.toList());
+    System.out.println(upper);
 
   }
 }
